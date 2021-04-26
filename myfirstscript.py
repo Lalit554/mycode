9221b89f8f29868f7fabf9ecae46fdc2a3400e62
Lalit Patil <lalit.patil@fcagroup.com>
2021-04-26 15:36:30 +0000

    learning about input statement

---
+++ myfirstscript.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+hello = "This again? Come up with something else Chad"
+
+num1 = 56
+
+num2 = 67.3
+
+print(hello)
+print("hello")
+print(num1 , num2)
+print(num1 + num2)
+print ( hello + "  hello" )
