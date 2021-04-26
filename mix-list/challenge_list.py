4534348d5cec7fea2de82b53fac6876b5a91497f
Lalit Patil <lalit.patil@fcagroup.com>
2021-04-26 17:26:40 +0000

    mixed lists

---
+++ mix-list/challenge_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print ("IP addresses in the list are: \n")
+print (iplist[3] + "\n")
+print (iplist[4] + "\n")
+
---
+++ mix-list/mixlist01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+my_list = [ "192.168.0.5" , 5060, "UP" ]
+
+print("The first item in the list (IP): " + my_list[0] )
+
+print("The first item in the list (IP): " + str(my_list[1]) )
+
+print("The first item in the list (IP): " + my_list[2] )
+
+
